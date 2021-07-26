288b16c0477d40cc861e08282ff0fd987cda155a
user <rallakhverdiev.exceedteam@gmail.com>
2021-07-26 18:20:47 +0300

    Add deleteAll (1)

--- app.js
+++ app.js
@@ -47,6 +47,15 @@ app.delete('/deleteOne', (req, res) => {
   });
 });
 
+// Удаление всех записей
+app.delete('/deleteAll', (req, res) => {
+  Task.remove({}).then(() => {
+    Task.find().then(result => {
+      res.send({data: result});
+    });
+  });
+});
+
 // Редактирование одной записи
 app.patch('/updateOne', (req, res) => {
   Task.updateOne({ _id: req.body._id }, req.body).then(() => {
